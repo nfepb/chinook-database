28366bd31eed2b13d8a8ab2c0ff85619a160b08b
nfepb <nicolas.planteb@gmail.com>
2022-09-23 18:20:46 +0000

    Adds example queries using SQLAlchemy's full ORM class-based models

--- sql-orm.py
+++ sql-orm.py
@@ -23,7 +23,7 @@ class Album(base):
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
     ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
-    
+
 
 # Create variable for "Track" table
 class Track(base):
@@ -47,3 +47,42 @@ session = Session()
 
 # creating the databse using declarative_base subclass
 base.metadata.create_all(db)
+
+# Query 1 - select all records from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+#     print(artist.Name)
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# artistId = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artistId.ArtistId, artistId.Name, sep=" | ")
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.ArtistId, album.Title, album.AlbumId, sep=" | ")
+
+# Query 6 - select all tracks where the composer is "Queen" from the
+# "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+        )
