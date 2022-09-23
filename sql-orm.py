e2c6b989617ef99f9580effb601b2b4d3ebffcff
nfepb <nicolas.planteb@gmail.com>
2022-09-23 17:33:17 +0000

    Adds sql-orm.py with instructions to access db, classes and initiate session

---
+++ sql-orm.py
@@ -0,0 +1,48 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# create variable for "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(string)
+
+
+# Create variable for "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+# Create variable for "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name = Column(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(String)
+    Bytes = Column(String)
+    UnitPrice = Column(Float)
+
+
+# instead of connecting to the database directly, we will ask for a session
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling to Session() subclass defined above
+session = Session()
+
+# creating the databse using declarative_base subclass
+base.metadata.create_all(db)
