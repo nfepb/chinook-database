b8441bfa9cc625fb749f8ad349f7ada38f34a928
nfepb <nicolas.planteb@gmail.com>
2022-09-24 13:36:20 +0000

    Removes import from float & foreign key

--- sql-crud.py
+++ sql-crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    create_engine, Column, Float, ForeignKey, Integer, String
+    create_engine, Column, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
