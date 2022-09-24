de7ef4c5a6cb0ad6052cf4f0299b6b270eb24ef1
nfepb <nicolas.planteb@gmail.com>
2022-09-24 14:28:16 +0000

    Adds logic to delete single record with defensive programming in sql-crud.py

--- sql-crud.py
+++ sql-crud.py
@@ -88,11 +88,55 @@ james_gosling = Programmer(
 )
 
 # add each instance of our programmers to our session
-session.add()
+# session.add(james_gosling)
+# session.add()
 
 
 # commit our session to the database
-session.commit()
+# session.commit()
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=12).first()
+# programmer.famous_for = "World President"
+
+
+# commit our session to the database
+# session.commit()
+
+
+# updating a single record
+# people = session.query(Programmer)
+# for person in people:
+#     if person.gender == "F":
+#         person.gender = "Female"
+#     elif person.gender == "M":
+#         person.gender = "Male"
+#     else:
+#         print("Gender not defined")
+#     session.commit()
+
+
+# deleting a single record
+fname = input("Enter a name:")
+lname = input("Enter a name:")
+programmer = session.query(Programmer).filter_by(
+    first_name=fname, last_name=lname
+    ).first()
+# defensive programming
+if programmer is not None:
+    print(
+        "Programmer Found: ",
+        programmer.first_name + " " + programmer.last_name
+        )
+    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    if confirmation.lower() == "y":
+        session.delete(programmer)
+        session.commit()
+        print("Programmer has been deleted")
+    else:
+        print("Programmer not deleted")
+else:
+    print("No records found")
 
 # query the database to find all the programmers
 programmers = session.query(Programmer)
