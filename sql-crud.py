1af14c78dca78e04321d6df06437b3187aae17f3
nfepb <nicolas.planteb@gmail.com>
2022-09-24 13:45:19 +0000

    Adds programmer class in sql-crud along with first entry of ada_lovelace

--- sql-crud.py
+++ sql-crud.py
@@ -9,6 +9,18 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
+
+# create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
 # instead of connecting to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
 Session = sessionmaker(db)
@@ -17,3 +29,12 @@ session = Session()
 
 # creating the databse using declarative_base subclass
 base.metadata.create_all(db)
+
+# creating records on our Programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="Frist Programmer"
+)
